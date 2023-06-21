034b7e042645c04b8bacf4de183ae93887fbfb1f
mosesokemwa <moses@untapped-global.com>
2023-06-21 13:45:50 +0300

    Here is a workaround to handle when gtm throw error when loading script and handle when it's loaded. What do you think about it ?
    SImply handle it with window.addEventListener("gtm_loading_error", func) and window.addEventListener("gtm_loaded", func)
    
    contribution by [DorianMaliszewski](https://github.com/DorianMaliszewski)

--- src/Snippets.js
+++ src/Snippets.js
@@ -19,6 +19,14 @@ const Snippets = {
         w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
         var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
         j.async=true;j.src='${gtm_src}/gtm.js?id='+i+dl+'${gtm_auth}${gtm_preview}&gtm_cookies_win=x';
+        j.addEventListener('error', function() {
+          var _ge = new CustomEvent('gtm_loading_error', { bubbles: true });
+          d.dispatchEvent(_ge);
+        });
+        j.addEventListener('load', function() {
+            var _ge = new CustomEvent('gtm_loaded', { bubbles: true });
+            d.dispatchEvent(_ge);
+        });
         f.parentNode.insertBefore(j,f);
       })(window,document,'script','${dataLayerName}','${id}');`
 
@@ -37,4 +45,4 @@ const Snippets = {
   }
 }
 
-module.exports = Snippets
+module.exports = Snippets;
